5afc4fdd613d0c1ba8038d15a97ec512015529a1
Chris Simeon <christian-herberholz@hotmail.de>
2024-01-03 17:27:41 +0100

    Restart Button works.

--- Timer.py
+++ Timer.py
@@ -1,7 +1,6 @@
 
 import tkinter as tk
 from tkinter import ttk
-import time
 import sys
 import os
 
@@ -10,61 +9,58 @@ HEIGHT = 2
 WIDTH = 10
 
 
+import tkinter as tk
 
+class App(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
 
-def buttonpress():
-    buttonpressed = True
-    print ("Yes")
+        # Build up the UI
+        self.label = tk.Label(root, height=HEIGHT, width=WIDTH, font=(
+            "Impact", 35), bd=5, fg="white", bg="black", text="hey")
+        self.label.pack(expand=True)
 
-def countdown():
-    query = sys.argv[1]
-    #query = 1
-    zeit = int(query)*60
-    global buttonpressed
-    buttonpressed= False
+       # Establish the Variable and tell Tkinter to watch it 
+        startButton = tk.Button(root, text="Restart", font=40, command=self.buttonpress)
+        startButton.pack(expand= True, fill="x")
 
-    def timetime(zeit):
-        if zeit >= 0:
-            m, s = divmod(zeit, 60)
+        # Initialize Button to False
+
+        # Pass on arguments from Shell
+        self.query = sys.argv[1]
+        self.zeit = int(self.query)*60
+
+        self.countdown()
+
+    def buttonpress(self): # resets Time to original time
+        self.zeit =  int(self.query)*60
+
+    def countdown(self):
+        if self.zeit >= 0:
+            m, s = divmod(self.zeit, 60)
             h, m = divmod(m, 60)
-            zeit = zeit - 1
-            label.config(text=str(h).zfill(2) + ":" +
+            self.zeit -= 1
+            self.label.config(text=str(h).zfill(2) + ":" +
                         str(m).zfill(2) + ":" + str(s).zfill(2))
-            if not buttonpressed:
-                label.after(1000, timetime(zeit))
-            else: 
-                buttonpressed = False
-                label.after(1000, timetime(int(query)*60))
+            print(self.buttonpressed.get())
+            self.label.after(1000, lambda: self.countdown())
         else:
             file = "./Alarm.mp3"
             os.system("mpg123 " + file)
-            zeit = 0
-            overtime(zeit)
+            self.overtime()
+    
 
-    def overtime(zeit):
-        m, s = divmod(zeit, 60)
-        h, m = divmod(m, 60)
-        zeit = zeit + 1
-        label.config(text=str(h).zfill(2) + ":" +
-                     str(m).zfill(2) + ":" + str(s).zfill(2))
-        label.after(1000, lambda: overtime(zeit))
+    def overtime(self):
+            m, s = divmod(self.zeit, 60)
+            h, m = divmod(m, 60)
+            self.zeit = self.zeit + 1
+            self.label.config(text=str(h).zfill(2) + ":" +
+                        str(m).zfill(2) + ":" + str(s).zfill(2))
+            self.label.after(1000, lambda: self.overtime())
 
-    timetime(zeit)
-
-# Grundfenster
 root = tk.Tk()
+myapp = App(root)
 root.title("Timer")
+myapp.mainloop()
 
-# Inhalt
-label = tk.Label(root, height=HEIGHT, width=WIDTH, font=(
-    "Impact", 35), bd=5, fg="white", bg="black", text="hey")
-label.pack(expand=True)
-
-restartButton = tk.Button(root, text="Restart", font=40, command=buttonpress)
-restartButton.pack(expand= True, fill="x")
-
-
-countdown()
-
-
-root.mainloop()
